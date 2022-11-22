58ebd41a18eee4598c56c8d803dbb066a7d44e40
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-11-23 02:15:10 +0800

    Add exercise files

---
+++ Chp1_Exercises/src/MovieQuote.java
@@ -0,0 +1,10 @@
+// MovieQuote.java
+// Chapter 1, Exercise 6
+// Display a favourite movie quote
+public class MovieQuote
+{
+    public static void main(String[] args)
+    {
+        System.out.println("On Wednesdays we wear pink."); //Display the string
+    }
+}
---
+++ Chp1_Exercises/src/MovieQuoteInfo.java
@@ -0,0 +1,13 @@
+// MovieQuoteInfo.java
+// Chapter 1,Exercise 7
+// Displays quote, movie, character, and year
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("On Wednesdays we wear pink.");
+        System.out.println("Mean Girls");
+        System.out.println("Karen Smith");
+        System.out.println("2004");
+    }
+}
